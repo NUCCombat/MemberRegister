2f11cf521c8a5147172c051dfa03e807b92b64a9
Oumourin <maolinwang1998@gmail.com>
2019-09-21 01:31:55 +0800

    添加初始管理员

--- run-production.py
+++ run-production.py
@@ -3,6 +3,9 @@ from MemberRegister import db
 from MemberRegister.model import *
 
 db.create_all()
+new_admin = Admin("NUCC", "1145141919")
+db.session.add(new_admin)
+db.session.commit()
 
 if __name__ == '__main__':
     from MemberRegister import app
